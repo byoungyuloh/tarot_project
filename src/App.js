aeb92ff31654d14f33de317bcd508138026761dc
byoungyuloh <111040429+byoungyuloh@users.noreply.github.com>
2022-12-12 11:13:18 +0900

    코드정리
    1. 드래그 & 드랍 시 움직이는 효과 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import card_image from './image/cd_image.png';
+import { useState } from 'react';
 
 function App() {
+  const [move, setMove] = useState(false);
+  const startMove = () => {
+    console.log('start');
+    setMove(true);
+  }
+  const endMove = () => {
+    setMove(false);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={ card_image } className = { move ? 'App-logo' : 'App-stop' } alt='logo' onDragOver={ startMove } onDragEnd = { endMove }/>
       </header>
+      {/* <button onClick = { startMove } style = { { width : '100px' } }>Start</button>
+      <button onClick = { endMove } style = { { width : '100px' } }>End</button> */}
     </div>
   );
 }
